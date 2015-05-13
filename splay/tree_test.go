80d9d6df1deca34fac4c3370b3554e02d9627d5b
Valerio.Gheri <valerio.gheri@gmail.com>
2015-05-13 19:35:53 +0200

    Improved test

--- splay/tree_test.go
+++ splay/tree_test.go
@@ -10,6 +10,27 @@ func createEmptyTree() Tree {
 	return *fakeTree
 }
 
+func createTreeWithRoot() Tree {
+	fakeTree := new(Tree)
+	fakeTree.Insert("myRoot", "{'test': 'value'}")
+	return *fakeTree
+}
+
+func createPopulatedTree() Tree {
+	fakeTree := new(Tree)
+	fakeTree.Insert("Abc", "{'test': 'value_Abc'}")
+	fakeTree.Insert("myRoot", "{'test': 'value'}")
+	fakeTree.Insert("myRoot", "{'test': 'value'}")
+	fakeTree.Insert("myRoot", "{'test': 'value'}")
+	fakeTree.Insert("myRoot", "{'test': 'value'}")
+	fakeTree.Insert("myRoot", "{'test': 'value'}")
+	fakeTree.Insert("myRoot", "{'test': 'value'}")
+	fakeTree.Insert("myRoot", "{'test': 'value'}")
+	fakeTree.Insert("myRoot", "{'test': 'value'}")
+	fakeTree.Insert("myRoot", "{'test': 'value'}")
+	return *fakeTree
+}
+
 func TestInsertRootCorrectly(t *testing.T) {
 	fakeTree := createEmptyTree()
 	fakeKey := "myRoot"
@@ -24,7 +45,53 @@ func TestInsertRootCorrectly(t *testing.T) {
 	if fakeTree.root.value == nil {
 		t.Error("Expecting value not nil, found nil")
 	}
-	if fakeTree.root.value != "{'test': 'value'}" {
+	if fakeTree.root.value != fakeValue {
 		t.Errorf("Expecting value: %s, found %s", fakeValue, fakeTree.root.value)
+	}
+}
+
+func TestInsertNodeOnLeftOfRoot(t *testing.T) {
+	fakeTree := createTreeWithRoot()
+	fakeKey := "left"
+	fakeValue := "{'test': 'value2'}"
+	fakeTree.Insert(fakeKey, fakeValue)
+	if fakeTree.root.right != nil {
+		t.Error("Expecting root.right to be nil, it is not.")
+	}
+	node := fakeTree.root.left
+	if node == nil {
+		t.Error("Left node not added")
+	}
+	if node.key != fakeKey {
+		t.Errorf("Expecting key: %s, found %s", fakeKey, node.key)
+	}
+	if node.value == nil {
+		t.Errorf("Expecting value %s, found nil", fakeValue)
+	}
+	if node.value != fakeValue {
+		t.Errorf("Expecting value: %s, found %s", fakeValue, node.value)
+	}
+}
+
+func TestInsertNodeOnRightOfRoot(t *testing.T) {
+	fakeTree := createTreeWithRoot()
+	fakeKey := "right"
+	fakeValue := "{'test': 'value2'}"
+	fakeTree.Insert(fakeKey, fakeValue)
+	if fakeTree.root.left != nil {
+		t.Error("Expecting root.left to be nil, it is not.")
+	}
+	node := fakeTree.root.right
+	if node == nil {
+		t.Error("Right node not added")
+	}
+	if node.key != fakeKey {
+		t.Errorf("Expecting key: %s, found %s", fakeKey, node.key)
+	}
+	if node.value == nil {
+		t.Errorf("Expecting value %s, found nil", fakeValue)
+	}
+	if node.value != fakeValue {
+		t.Errorf("Expecting value: %s, found %s", fakeValue, node.value)
 	}
 }
