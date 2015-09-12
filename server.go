4044b31ff29e997491ae7697f821b9a77465ee1b
Valerio Gheri <valerio.gheri@gmail.com>
2015-09-13 00:02:59 +0200

    Basic POST support. Need to completely refactor this code into something nice, with better error handling and error management

--- server.go
+++ server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/vgheri/goCacheIt/Godeps/_workspace/src/github.com/gorilla/mux"
 	"github.com/vgheri/goCacheIt/splay"
 	"log"
@@ -11,13 +10,25 @@ import (
 
 var dataStore *splay.Tree
 
+// KeyValueCouple models the input data for the POST request
+type KeyValueCouple struct {
+	Key   string    //`json:"key"`
+	Value splay.Any //`json:"value"`
+}
+
+// APIError models the error object sent back to the client on error
+type APIError struct {
+	Code    int
+	Message string
+}
+
 func main() {
 	log.Println("Starting server...")
 	log.Println("Initializing data store...")
 	dataStore = splay.New()
 	server := mux.NewRouter()
-	server.HandleFunc("/test/{id}", handleTest).
-		Methods("GET")
+	server.HandleFunc("/api/v1/store/", handlePostValueByKey).
+		Methods("POST")
 	server.HandleFunc("/api/v1/store/{key}", handleGetValueByKey).
 		Methods("GET")
 
@@ -25,18 +36,9 @@ func main() {
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
-func handleTest(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-	log.Printf("Inside the handler. Id %s", id)
-	fmt.Fprintf(w, "Hello id %s!", id)
-}
-
 func handleGetValueByKey(w http.ResponseWriter, r *http.Request) {
-	//log.Print("Inside handleGetValueByKey.")
 	vars := mux.Vars(r)
 	key := vars["key"]
-	//log.Printf("Key %s", key)
 	node, err := dataStore.Get(key)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -51,5 +53,35 @@ func handleGetValueByKey(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	w.Header().Set("Content-Type", "application/json")
 	w.Write(value)
+}
+
+func handlePostValueByKey(w http.ResponseWriter, r *http.Request) {
+	var reqBody KeyValueCouple
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&reqBody); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422) // unprocessable entity
+		apiErr := APIError{Message: err.Error()}
+		if err := json.NewEncoder(w).Encode(apiErr); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	if reqBody.Key == "" /*|| len(reqBody.Value) == 0*/ {
+		http.Error(w, "Please specify a valid key and a value",
+			http.StatusBadRequest)
+		return
+	}
+	_, err := dataStore.Insert(reqBody.Key, reqBody.Value)
+	if err != nil {
+		http.Error(w, err.Error(),
+			http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
