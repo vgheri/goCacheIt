c91b9fc56fc64443a3163c4fde4e2c1587c6cfb0
Valerio Gheri <valerio.gheri@gmail.com>
2015-11-22 14:48:32 +0100

    Added missing files

---
+++ handlers/handlers_test.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"testing"
+)
+
+func TestMemoryUsage(t *testing.T) {
+	// tree := createFixedTree()
+	// for {
+	// 	var key string
+	// 	for {
+	// 		key = randSeq(5)
+	// 		if node, _ := tree.Get(key); node == nil {
+	// 			break
+	// 		}
+	// 	}
+	// 	_, _ = tree.Insert(key, "{'test': 'asdasdadasasdasdadasasdasdadasasdasdadasasdasdadasasdasdadas'}", 500*time.Second)
+	// }
+	// close(tree.jobs)
+}
